ded3acaa3fff32dcc06fa066b8ef61b2ec3ccf19
henrique <7henrique18@gmail.com>
2025-07-12 12:02:10 -0300

    feat: Tailwind CSS and date-fns integration
    - Added Tailwind CSS for styling and configured Vite to use it.
    - Integrated date-fns for date formatting in the app.
    - Updated package.json to include new dependencies: date-fns and tailwindcss.
    - Refactored App.tsx to display weather information with Tailwind styles.
    - Created index.css for Tailwind theming.
    - Added a cloudy image asset for weather representation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
+import { format } from "date-fns";
+import cloudy from "./assets/cloudy.png";
+
+const WEATHERS = [
+	{
+		id: 1,
+		name: "Suzano",
+		description: "Cloudy",
+		temperature: 20,
+		image: cloudy,
+		high: 25,
+		low: 15,
+	},
+];
+
 function App() {
 	return (
-		<>
-			<h1>weather app</h1>
-		</>
+		<div className="flex flex-col items-center justify-center h-screen bg-background text-primary">
+			<section className="space-y-4 max-w-md mx-auto w-full">
+				{WEATHERS.map((weather) => (
+					<div
+						key={weather.id}
+						className="bg-red-900 rounded-3xl p-4 w-full relative overflow-hidden"
+					>
+						<img
+							src={weather.image}
+							alt=""
+							className="w-full h-full object-cover absolute top-0 left-0"
+						/>
+
+						<div className="relative z-10 flex flex-col gap-4">
+							<div className="flex items-center justify-between">
+								<div className="">
+									<h1 className="text-primary text-2xl font-bold text-shadow-lg">
+										{weather.name}
+									</h1>
+
+									<p className="text-primary text-sm">
+										{format(new Date(), "HH:mm")}
+									</p>
+								</div>
+
+								<p className="text-primary text-5xl text-shadow-lg">
+									{weather.temperature}°C
+								</p>
+							</div>
+
+							<div className="flex items-center justify-between">
+								<p className="text-primary text-shadow-lg font-semibold">
+									{weather.description}
+								</p>
+
+								<p className="text-primary text-shadow-md font-medium">
+									H:{weather.high}°C / L:{weather.low}°C
+								</p>
+							</div>
+						</div>
+					</div>
+				))}
+			</section>
+		</div>
 	);
 }
 

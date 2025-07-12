07814ca28df046d7c83c7e203ecf8fb24d828b50
henrique <7henrique18@gmail.com>
2025-07-12 14:36:48 -0300

    chore: update OpenWeather API key management to use environment variable

--- src/services/open-weather.ts
+++ src/services/open-weather.ts
@@ -6,7 +6,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const openWeatherApi = axios.create({
 	baseURL: BASE_URL,
 	params: {
-		appid: "fe017193ff86dce749f9e4c7f4f5ff5f",
+		appid: import.meta.env.VITE_OPEN_WEATHER_API_KEY,
 	},
 });
 

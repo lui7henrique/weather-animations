dd83fa17c3e4ea9af46642486f8c064dcc075bd9
henrique <7henrique18@gmail.com>
2025-07-12 14:41:25 -0300

    feat: add WeatherDetails component and streamline OpenWeather API key usage

--- src/services/open-weather.ts
+++ src/services/open-weather.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 import type { WeatherResponse } from "../types/weather";
 
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+const appid = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
+console.log(appid);
 
 const openWeatherApi = axios.create({
 	baseURL: BASE_URL,
 	params: {
-		appid: import.meta.env.VITE_OPEN_WEATHER_API_KEY,
+		appid,
 	},
 });
 
